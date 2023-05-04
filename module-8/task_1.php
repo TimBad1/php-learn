1da2a4221a514668849fbfa5eadd866f941e4914
Timur Spektrow <timur@studio-aw.ru>
2023-05-04 21:33:52 +0300

    fix: storeText, loadText, editText

--- module-8/task_1.php
+++ module-8/task_1.php
@@ -13,8 +13,8 @@
         public function showTelegraph() {
             echo $this->author . ' ' . $this->slug . ' ' . $this->published . PHP_EOL;
     }
-        public function storeText($text, $title) {
-            $entry = [ 'text' => $text, 'title' => $title, 'author' => $this->author, 'published' => date("Y-m-d H:i:s")];
+        public function storeText() {
+            $entry = [ 'text' => $this->text, 'title' => $this->title, 'author' => $this->author, 'published' => $this->published];
             $serFile = serialize($entry);
             file_put_contents($this->slug, $serFile);
         }
@@ -23,7 +23,10 @@
             if (file_exists($slug)) {
                 $file = file_get_contents($slug);
                 $unserFile = unserialize($file);
-//                print_r($unserFile);
+                $this->title = $unserFile['title'];
+                $this->text = $unserFile['text'];
+                $this->author = $unserFile['author'];
+                $this->published = $unserFile['published'];
                 return $unserFile['text'];
 
             } else {
@@ -32,26 +35,15 @@
         }
 
         public function editText($text, $title) {
-            if (file_exists($this->slug)) {
-                $file = file_get_contents($this->slug);
-                $unserFile = unserialize($file);
-                $unserFile['text'] = $text;
-                $unserFile['title'] = $title;
-                $unserFile['published'] = date("Y-m-d H:i:s");
-                $serFile = serialize($unserFile);
-                file_put_contents($this->slug, $serFile);
-            } else {
-                $this->storeText($text, $title);
-            }
-
-            //$entry = [ 'text' => $text, 'title' => $title, 'author' => $this->author, 'published' => date("Y-m-d H:i:s")];
+                $this->text = $text;
+                $this->title = $title;
         }
     }
 
     $telegraph = new TelegraphText('Pushkin', 'test_text_file');
 //    $telegraph->showTelegraph();
     $telegraph->editText('Ещё более новая запись', 'Супер-заголовок');
-    $telegraph->storeText('новая запись', 'Заголовок новой записи');
+    $telegraph->storeText();
     $telegraph->loadText('test_text_file');
 
 
